8c3499340303d0f8351aba35e86b2618c21d9acb
Jaden Holladay <holladaystudio@gmail.com>
2017-02-16 20:07:01 -0700

    Remove DateTime

--- warehouse_unittest.cpp
+++ warehouse_unittest.cpp
@@ -97,54 +97,6 @@ TEST(Test_Functionality, Expired_Removal_2)
 	EXPECT_EQ(1, test_warehouse.Get_Inventory().find("0237")->second.size());
 }
 
-// +++++++++++++++++++ Test DateTime +++++++++++++++++++
-
-/* NOTES ON time.h
-*
-* Uses the local time zone of the machine running the code.
-* this shouldn't be a problem since we're not worried
-* about hours or minutes.
-*
-* Need to fully initialize a struct tm to be empty before
-* converting into it.
-*/
-
-#include "DateTime.cpp"
-
-// Converts a datetime to a UTC time code
-TEST(Test_DateTime, Convert_UTC)
-{
-	int utcTime = DateTime::ConvertToUTC("1996/12/11");
-
-  EXPECT_EQ(850287600, utcTime);
-}
-
-//Assert that the timestamps for two different dates are 1 day apart from eachother when converted to utc
-TEST(Test_DateTime, UTC_Date_Difference)
-{
-	std::string dateA = "1996/12/11";
-	std::string dateB = "1996/12/12";
-
-	int utcTimeA = DateTime::ConvertToUTC(dateA);
-	int utcTimeB = DateTime::ConvertToUTC(dateB);
-
-  //Ensure that the length of time between the two dates is
-  //86400 (the length of one day)
-  EXPECT_EQ(utcTimeB - utcTimeA, 86400);
-  EXPECT_EQ(DateTime::DaysBetween(dateA,dateB), 1);
-  EXPECT_EQ(DateTime::DaysBetween(utcTimeA, utcTimeB), 1);
-}
-
-// Tests the DateTime implementation on a pretty big range of dates (my lifetime so far!)
-// Demonstrates that the DateTime functions correctly account for leap years
-TEST(Test_DateTime, UTC_Big_Date_Difference)
-{
-	std::string dateA = "1996/12/11";
-	std::string dateB = "2017/02/14";
-
-  EXPECT_EQ(DateTime::DaysBetween(dateA,dateB), 7370);
-}
-
 // ##################### STEP 3 ##################
 // Run all the defined tests by calling RUN_ALL_TESTS() in main().
 // This is done for us by linking gtest_main.cc through our makefile.
